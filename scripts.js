bf2adb027566f79b1d837da1d3d9ba4619b18209
mikaele2222 <mfs52@aluno.ifal.edu.br>
2024-11-21 11:17:30 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function verificarMaior() {
+    
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var num3 = parseInt(document.getElementById("num3").value);
+
+
+    if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
+        alert("Por favor, insira três números válidos.");
+        return;
+    }
+
+    var maior = num1;
+
+    if (num2 > maior) {
+        maior = num2;
+    }
+    if (num3 > maior) {
+        maior = num3;
+    }
+
+ 
+    document.getElementById("resultado").innerText = "O maior número é: " + maior;
+}
